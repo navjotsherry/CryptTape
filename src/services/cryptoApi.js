53cf028cddf7006972d28f8ac06aecdab50ae882
navjotsherry <85787077+navjotsherry@users.noreply.github.com>
2022-03-07 07:34:22 +0530

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
-  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-  'x-rapidapi-key': 'fee93448b9msh80d1afcbed37d02p1a4e73jsn673658f46b7c',
+  'x-rapidapi-host': '-----',
+  'x-rapidapi-key': '----',
 }
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
